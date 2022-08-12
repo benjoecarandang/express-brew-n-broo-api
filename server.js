00329e8a5ce6f97d6f37a91b4b6413a7b3f1846f
Benjoe Oates Carandang <benjoe.carandang@gmail.com>
2022-08-12 21:09:44 +0800

    initialize connection to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose.connect(DB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.error.bind(console, "MongoDB connection error:");
+}).on("connected", () => {
+  console.log("Connected in Mongoose DB");
+});
 
 const port = 8000;
 
